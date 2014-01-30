b4dfa0e3f1b4fe3800c67ed761e542fc86b6ea87
vanderkyl <vandermail@utexas.edu>
2014-01-30 17:28:35 -0600

    First commit

---
+++ Ogre/BallSim.h
@@ -0,0 +1,70 @@
+#ifndef __BallSim_h_
+#define __BallSim_h_
+ 
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+ 
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+ 
+#include <SdkTrays.h>
+#include <SdkCameraMan.h>
+
+class BallSim : public Ogre::FrameListener, public Ogre::WindowEventListener, public OIS::KeyListener, public OIS::MouseListener, OgreBites::SdkTrayListener
+{
+public:
+    BallSim(void);
+    virtual ~BallSim(void);
+    void move(const Ogre::FrameEvent& evt);
+    bool go(void);
+    void createBox(void);
+protected:
+    Ogre::SceneNode* sunNode;
+    Ogre::Vector3 sunDirection;
+    Ogre::Real sunRadius;
+    Ogre::Real sunSpeed;
+
+    Ogre::Root *mRoot;
+    Ogre::Camera* mCamera;
+    Ogre::SceneManager* mSceneMgr;
+    Ogre::RenderWindow* mWindow;
+    Ogre::String mResourcesCfg;
+    Ogre::String mPluginsCfg;
+ 
+    // OgreBites
+    OgreBites::SdkTrayManager* mTrayMgr;
+    OgreBites::SdkCameraMan* mCameraMan;      // basic camera controller
+    OgreBites::ParamsPanel* mDetailsPanel;    // sample details panel
+    bool mCursorWasVisible;                   // was cursor visible before dialog appeared
+    bool mShutDown;
+ 
+    // OIS Input devices
+    OIS::InputManager* mInputManager;
+    OIS::Mouse*    mMouse;
+    OIS::Keyboard* mKeyboard;
+ 
+    // Ogre::FrameListener
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+ 
+    // OIS::KeyListener
+    virtual bool keyPressed( const OIS::KeyEvent &arg );
+    virtual bool keyReleased( const OIS::KeyEvent &arg );
+    // OIS::MouseListener
+    virtual bool mouseMoved( const OIS::MouseEvent &arg );
+    virtual bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+    virtual bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+ 
+    // Ogre::WindowEventListener
+    virtual void windowResized(Ogre::RenderWindow* rw);
+    virtual void windowClosed(Ogre::RenderWindow* rw);
+};
+ 
+#endif // #ifndef __BallSim_h_
